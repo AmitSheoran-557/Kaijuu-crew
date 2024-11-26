6d90eaba6a4b33d92ea066683a76eef0fd6795de
AmitSheoran-557 <amit22082006@gmail.com>
2024-11-26 14:32:45 +0530

    slider setion

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,34 @@
+// ==================================== Nav bar js ===============================================
+const menuicon = document.querySelector(".menubar");
+const menuList = document.querySelector(".menulist");
+const menubarFirst = document.querySelector(".menubarFirst");
+const menubarSecond = document.querySelector(".menubarSecond");
+const menubarThird = document.querySelector(".menubarThird");
+const body = document.body;
+
+menuicon.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+    if (window.innerWidth < 1024) {
+        menuList.classList.toggle("-right-full");
+        menuList.classList.toggle("right-0");
+        menubarFirst.classList.toggle("rotate-[45deg]");
+        menubarFirst.classList.toggle("-translate-y-[1.5px]");
+        menubarSecond.classList.toggle("hidden");
+        menubarThird.classList.toggle("rotate-[-45deg]");
+        menubarThird.classList.toggle("translate-y-[1.5px]");
+        body.classList.toggle("overflow-hidden");
+    }
+}
+const menuItems = document.querySelectorAll(".menulist button, .menulist a");
+menuItems.forEach(item => {
+    item.addEventListener("click", () => {
+        if (window.innerWidth < 992) {
+            toggleMenu();
+        }
+    });
+});
+
 // ==================================== Timer js  ==============================================================
 let totalSeconds = (10 * 24 * 3600) + (20 * 3600) + (15 * 60) + 10;
 
@@ -48,7 +79,6 @@ function toggleAccordion(element, forceOpen = false) {
 
     if (forceOpen || content.classList.contains('hidden')) {
         content.classList.remove('hidden');
-        element.classList.add('bg-gray-700');
         icon.style.transform = 'rotate(180deg)';
     } else {
         content.classList.add('hidden');
@@ -56,7 +86,26 @@ function toggleAccordion(element, forceOpen = false) {
         icon.style.transform = 'rotate(0deg)';
     }
 }
- //========================= JavaScript to set the current year =========================
- const yearElement = document.getElementById('currentYear');
- const currentYear = new Date().getFullYear();
- yearElement.textContent = currentYear;
+//========================= JavaScript to set the current year =========================
+
+const yearElement = document.getElementById('currentYear');
+const currentYear = new Date().getFullYear();
+yearElement.textContent = currentYear;
+
+//  ================================= Slider js =========================================
+$(".card-slider").slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: $(".slick-prev"),
+    nextArrow: $(".slick-next"),
+    responsive: [
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+            },
+        },
+    ],
+});
